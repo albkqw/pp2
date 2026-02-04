29bc6335b2bdd6e9b80cbb85fbd494056806c3aa
albkqw <a.abduldinov@bk.ru>
2026-02-04 20:55:47 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice 2/boolean/boolean_intro.py
@@ -0,0 +1,21 @@
+# 1
+print(bool("Hello"))
+print(bool(15))
+
+# 2
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+# 3
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+# 4
+def myFunction() :
+  return True
+
+print(myFunction())
---
+++ Practice 2/loops/while_break.py
@@ -0,0 +1,15 @@
+# 1
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+
+# 2
+i = 0
+while i < 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
